fef90de9e2ee2259b70611bc6550775ac543639c
Arsene Juma <jumaaarsene@gmail.com>
2026-07-29 20:03:26 +0300

    Complete Case 2 Part A - Variable Initialization

---
+++ mpesa_reconciliation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+float cashBalance = 90000.0;
+float electronicFloat = 110000.0;
+
+float customerDeposits = 75500.0;
+float customerWithdrawals = 62300.0;
+
+float cashOperatingExpenses = 2000.0;
+
+float floatPurchase = 20000.0;
+float floatPurchaseFee = 200.0;
+
+float commission = 1850.0;
+
+float actualClosingCash = 80800.0;
+float actualClosingFloat = 118700.0;
+
+float permittedDifference = 100.0;
+
+    return 0;
+}
